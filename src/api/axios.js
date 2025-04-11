7a7e168f38488d24e0e30b706c525621e86fa958
Vapo30739 <abomariamvapo@gmail.com>
2025-04-11 22:23:47 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://abomariambackend.vercel.app/",
+  baseURL: "https://vapo-backend.vercel.app/",
   timeout: 300000,
   headers: {
     "Content-Type": "application/json",
