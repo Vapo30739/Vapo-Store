503a532130fb65d72ee18e3c48ed70ed2ec58db9
AhmadAl-Habal <80961574+AhmadAl-Habal@users.noreply.github.com>
2025-06-05 22:32:06 +0300

    Fixing the vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -8,8 +8,12 @@ createRoot(document.getElementById("root")).render(
   process.env.NODE_ENV === "development" ? (
     <StrictMode>
       <App />
+      <Analytics />
     </StrictMode>
   ) : (
-    <App />
+    <>
+      <App />
+      <Analytics />
+    </>
   )
 );
